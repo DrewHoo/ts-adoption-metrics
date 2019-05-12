984c4c84749629bcf296b9a2326d6bcfb4c7cba4
DrewHoo <drewhoover@gmail.com>
2019-05-12 00:11:47 -0500

    Find usages of discovered types

--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,122 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as readline from 'readline';
+import * as fs from 'fs'
+import * as path from 'path'
+import * as readline from 'readline'
 
+type Dict<K extends string, V> = { [key in K]: V }
 
-function main() {
-    debugger;
-    const pathname = '/Users/drew.hoover/Projects/novo/'
-    walk(pathname, /export interface\s([\w\d]*)[<\s]/).then(result =>
-        console.log(result, result.length))
-    walk(pathname, /([\w\d]*):\sany|([\w\d]*)=>\sany/).then(result =>
-        console.log(result, result.length))
+type WalkArgs = Dict<string, { regex: RegExp; matches: string[] }>
+
+async function main() {
+    const pathname = '/Users/DrewHoo/Atomic/archive/FAAC/ios/'
+    const interfaceUsagesPromise = findUsages(
+        pathname,
+        'interfaces',
+        /export interface\s([\w\d]*)[<\s,]/,
+        name =>
+            new RegExp(
+                `([\\w\\d\\s]*):\\s${name}|([\\w\\d\\s]*)\\s=>\\s${name}`
+            )
+    )
+
+    const typeUsagesPromise = findUsages(
+        pathname,
+        'types',
+        /export type\s([\w\d]*)[<\s,]/,
+        name =>
+            new RegExp(
+                `([\\w\\d\\s]*):\\s${name}|([\\w\\d\\s]*)\\s=>\\s${name}`
+            )
+    )
+    console.log(await interfaceUsagesPromise)
+    console.log(await typeUsagesPromise)
 }
 main()
-export async function walk(dir: fs.PathLike, regex: RegExp): Promise<string[]> {
-    const names: string[] = (await asyncReaddir(dir)).map(name => path.join(dir.toString(), name))
-    const stats: Array<fs.Stats> = await asyncMap(names, asyncStat);
 
-    const dict: { [name: string]: fs.Stats } = names.reduce((prev, next) => ({
-        ...prev,
-        [next]: stats.shift()
-    }), {});
-
-    const files = names.filter(n => dict[n].isFile()).filter(s => s.endsWith('.ts'));
-    const dirs = names.filter(n => dict[n].isDirectory())
-        .filter(s => !['node_modules', 'dist'].includes(path.basename(s)))
-        .filter(s => !path.basename(s).startsWith('.'));
-
-    const getMatches = async (file: fs.PathLike) => {
-        let thing;
-        try {
-            thing = await getMatchesInFile(file, regex);
-        } catch (error) {
-            console.log(error);
-        }
-        return thing
-    }
-    const getWalk = async (_dir: fs.PathLike) => await walk(_dir, regex);
-
-    return [
-        ...(await asyncMap(files, getMatches)),
-        ...(await asyncMap(dirs, getWalk)),
-    ].reduce((prev, next) => prev.concat(next), []);
+export async function findUsages(
+    pathname: fs.PathLike,
+    syntaxName: string,
+    syntaxRegex: RegExp,
+    usageRegex: (name: string) => RegExp
+): Promise<string[]> {
+    const matchDict = await walk(pathname, {
+        [syntaxName]: { regex: syntaxRegex, matches: [] },
+    })
+    const newMatchDict: WalkArgs = matchDict[syntaxName].matches.reduce(
+        (prev, next) => ({
+            ...prev,
+            [next]: {
+                regex: usageRegex(next),
+                matches: [],
+            },
+        }),
+        {}
+    )
+    const usages = await walk(pathname, newMatchDict)
+    return Object.keys(usages).map(
+        name => `${name}: ${usages[name].matches.length} ${usages[name].matches}`
+    )
 }
 
-export async function getMatchesInFile(file: fs.PathLike, regex: RegExp): Promise<string[]> {
-    const readable = fs.createReadStream(file);
-    const rl = readline.createInterface({
-        input: readable,
-        crlfDelay: Infinity
-    });
-    return new Promise((resolve, reject) => {
+export async function walk(
+    dir: fs.PathLike,
+    matchDict: WalkArgs
+): Promise<WalkArgs> {
+    const names: string[] = (await asyncReaddir(dir)).map(name =>
+        path.join(dir.toString(), name)
+    )
+    const stats: Array<fs.Stats> = await asyncMap(names, asyncStat)
 
-        const matches: string[] = []
+    const dict: { [name: string]: fs.Stats } = names.reduce(
+        (prev, next) => ({
+            ...prev,
+            [next]: stats.shift(),
+        }),
+        {}
+    )
+
+    const files = names
+        .filter(n => dict[n].isFile())
+        .filter(s => s.endsWith('.ts') || s.endsWith('.tsx'))
+    const dirs = names
+        .filter(n => dict[n].isDirectory())
+        .filter(s => !['node_modules', 'dist'].includes(path.basename(s)))
+        .filter(s => !path.basename(s).startsWith('.'))
+
+    const getMatches = async (file: fs.PathLike) =>
+        await getMatchesInFile(file, matchDict)
+    const getWalk = async (_dir: fs.PathLike) => await walk(_dir, matchDict)
+    await asyncMap(files, getMatches)
+    await asyncMap(dirs, getWalk)
+
+    return matchDict
+}
+
+export async function getMatchesInFile(
+    file: fs.PathLike,
+    matchDict: WalkArgs
+): Promise<WalkArgs> {
+    const rl = readline.createInterface({
+        input: fs.createReadStream(file),
+        crlfDelay: Infinity,
+    })
+    return new Promise((resolve, reject) => {
         rl.on('line', (line: string) => {
-            if (regex.test(line)) {
-                matches.push(`${regex.exec(line)[1]}`)
-            }
+            Object.keys(matchDict).forEach(name => {
+                if (matchDict[name].regex.test(line)) {
+                    matchDict[name].matches.push(
+                        `${matchDict[name].regex.exec(line)[1]}`
+                    )
+                }
+            })
         })
         rl.on('close', () => {
-            resolve(matches)
-        });
-    });
+            resolve()
+        })
+    })
 }
 
 function asyncMap<T, K>(array: T[], map: (arg: T) => Promise<K>): Promise<K[]> {
-    return Promise.all(array.map(map));
+    return Promise.all(array.map(map))
 }
 
 function asyncReaddir(dir: fs.PathLike): Promise<string[]> {
@@ -84,11 +136,11 @@ function asyncStat(path: fs.PathLike): Promise<fs.Stats> {
     return new Promise((resolve, reject) => {
         const callback = (err: NodeJS.ErrnoException, o: fs.Stats) => {
             if (err) {
-                reject(err);
+                reject(err)
             } else {
-                resolve(o);
+                resolve(o)
             }
         }
-        fs.stat(path, callback);
+        fs.stat(path, callback)
     })
 }
